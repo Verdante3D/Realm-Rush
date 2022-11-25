2285d36a176aac41f9f612b4033ca1d9a3fcd6f8
Verdante3D <Mikemcclain14@gmail.com>
2022-11-25 17:36:47 +0100

    Video 5.137 finished

--- Realm Rush/Assets/Pathfinding/GridManager.cs
+++ Realm Rush/Assets/Pathfinding/GridManager.cs
@@ -5,24 +5,33 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     [SerializeField] Vector2Int gridSize;
-Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
+    Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
 
-void Awake()
-{
-    CreateGrid();
-}
-
-void CreateGrid()
-{
-    for (int x = 0; x < gridSize.x; x++)
+    void Awake()
     {
-        for (int y = 0; y < gridSize.y; y++)
+        CreateGrid();
+    }
+
+    public Node GetNode(Vector2Int coordinates)
+    {
+        if (grid.ContainsKey(coordinates))
         {
-            Vector2Int coordinates = new Vector2Int(x, y);
-            grid.Add(coordinates, new Node(coordinates, true));
-            Debug.Log(grid[coordinates].coordinates + "=" + grid [coordinates].isWalkable);
+            return grid[coordinates];
+        }
+        return null;
+    }
+
+    void CreateGrid()
+    {
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                Vector2Int coordinates = new Vector2Int(x, y);
+                grid.Add(coordinates, new Node(coordinates, true));
+                Debug.Log(grid[coordinates].coordinates + "=" + grid[coordinates].isWalkable);
+            }
         }
     }
-}
 
 }
--- Realm Rush/Assets/Scripts/CoordinateLabeler.cs
+++ Realm Rush/Assets/Scripts/CoordinateLabeler.cs
@@ -10,17 +10,19 @@ public class CoordinateLabeler : MonoBehaviour
 {
     [SerializeField] Color defaultColor = Color.white;
     [SerializeField] Color blockedColor = Color.grey;
+    [SerializeField] Color exploredColor = Color.yellow;
+    [SerializeField] Color pathColor = new Color(1f, 0.5f, 0f);
 
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
-    Waypoint waypoint;
+    GridManager gridManager;
 
     void Awake()
     {
+        gridManager = FindObjectOfType<GridManager>();
         label = GetComponent<TextMeshPro>();
         label.enabled = false;
 
-        waypoint = GetComponentInParent<Waypoint>();
         DisplayCoordinates();
     }
 
@@ -31,7 +33,6 @@ public class CoordinateLabeler : MonoBehaviour
             DisplayCoordinates();
             //UpdateObjectName();
         }
-
         SetLabelColor();
         ToggleLabels();
     }
@@ -40,20 +41,35 @@ public class CoordinateLabeler : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.C))
         {
-
             label.enabled = !label.IsActive();
         }
     }
 
     void SetLabelColor()
     {
-        if (waypoint.IsPlacable)
-        {
-            label.color = defaultColor;
-        } else
+        if (gridManager == null) { return; }
+
+        Node node = gridManager.GetNode(coordinates);
+
+        if (node == null) { return; }
+
+        if (!node.isWalkable)
         {
             label.color = blockedColor;
         }
+        else if (node.isPath)
+        {
+            label.color = pathColor;
+        }
+        else if (node.isExplored)
+        {
+            label.color = exploredColor;
+        }
+        else
+        {
+            label.color = defaultColor;
+        }
+
     }
 
     void DisplayCoordinates()
