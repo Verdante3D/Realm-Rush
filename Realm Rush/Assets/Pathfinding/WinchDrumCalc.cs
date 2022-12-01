2ade7b84ccb40ce6d20dfb27c0af467321e1e844
Verdante3D <Mikemcclain14@gmail.com>
2022-12-01 18:14:37 +0100

    Video 5.139 finished

---
+++ Realm Rush/Assets/Pathfinding/WinchDrumCalc.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class WinchDrumCalc : MonoBehaviour
+{
+
+    [SerializeField] float drumSpindleDia = 20;
+    [SerializeField] float drumFlangeDia = 180;
+    [SerializeField] float ropeDia = 2.38f;
+    [SerializeField] float flangeWidth = 80;
+
+    float anyLayer;
+
+    //float firstLayer = drumSpindleDia + ropeDia;
+    //float standardLayer = (drumSpindleDia + drumFlangeDia) / 2;
+    [SerializeField] int wrapCount = 20;
+
+    private void Update()
+    {
+        for (int i = 0; i <= wrapCount; i++)
+        {
+            for (int j = 0; j <= wrapCount; j++)
+            {
+                anyLayer = Mathf.PI * (flangeWidth / ropeDia - 1) * (drumSpindleDia + ((2 * j - 1) * ropeDia)) / 1000;
+            Debug.Log($"Length layer {wrapCount}: {anyLayer}");
+            }
+
+            anyLayer += anyLayer;
+        }
+        Debug.Log($"Total length: {anyLayer}");
+    }
+}
