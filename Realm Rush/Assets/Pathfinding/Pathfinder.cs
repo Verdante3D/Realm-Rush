8e7cf8b4242df5489cc76c920e3cebeeb71a7779
Verdante3D <Mikemcclain14@gmail.com>
2022-12-08 11:20:24 +0100

    Video 5.144 finished

--- Realm Rush/Assets/Pathfinding/Pathfinder.cs
+++ Realm Rush/Assets/Pathfinding/Pathfinder.cs
@@ -131,4 +131,9 @@ public class Pathfinder : MonoBehaviour
 
         return false;
     }
+
+    public void NotifyReceivers()
+    {
+        BroadcastMessage("RecalculatePath", SendMessageOptions.DontRequireReceiver);
+    }
 }
